abec7ae82ab6d029462a5e0f954d2f8bdc1bfde6
JalmaGit <joar.m.breivik@pm.me>
2026-03-27 09:57:50 +0100

    fix: ubuntu 24 support

--- scripts/distro_selector.sh
+++ scripts/distro_selector.sh
@@ -6,7 +6,10 @@ echo "Detected: ${linux_distro}"
 
 case ${linux_distro} in
 	"Ubuntu Linux 22.04 (Jammy Jellyfish)") echo "Coming Soon...";;
-	"Ubuntu Linux 24.04 (Noble Numbat)") source scripts/distros/noble.sh;;
+	"Ubuntu 24.04.4 LTS") source scripts/distros/noble.sh;;
+	"Ubuntu 24.04.3 LTS") source scripts/distros/noble.sh;;
+	"Ubuntu 24.04.2 LTS") source scripts/distros/noble.sh;;
+	"Ubuntu 24.04.1 LTS") source scripts/distros/noble.sh;;
 	"Linux Mint 22"*) source scripts/distros/noble.sh;;
 	"Fedora Linux 43 (Workstation Edition)") source scripts/distros/fedora_43.sh;;
 	*) echo "Unsupported distribution: ${linux_distro}";;
